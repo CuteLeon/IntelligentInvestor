1bc211ebc00de51cfdb1e1a980c981e39db4a0c8
Leon Liu <lliu@fnz.com>
2022-01-09 00:41:58 +0800

    Add spider project;

---
+++ IntelligentInvestor.Spider/IStockSpider.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using IntelligentInvestor.Domain.Companys;
+using IntelligentInvestor.Domain.Quotas;
+using IntelligentInvestor.Domain.Stocks;
+
+namespace IntelligentInvestor.Spider;
+
+public interface IStockSpider
+{
+    Task<(Stock Stock, Quota Quota)> GetStockQuotaAsync(string code, StockMarkets market);
+
+    Task<Image> GetChartAsync(string code, StockMarkets market, QuotaFrequencys quotaFrequency);
+
+    Task<Company> GetCompanyAsync(string code, StockMarkets market);
+
+    Task<List<Quota>> GetQuotasAsync(string code, StockMarkets market, QuotaFrequencys quotaFrequency, DateTime fromDate, DateTime toDate);
+
+    Task<List<Stock>> GetHotStocksAsync();
+
+    Task<List<Stock>> SearchStocksAsync(string keyword);
+}
