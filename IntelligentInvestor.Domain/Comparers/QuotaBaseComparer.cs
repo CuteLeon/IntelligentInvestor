33d18006c7d0b812a013bbcefa1c3a9dace6a5b6
Leon Liu <lliu@fnz.com>
2022-01-08 22:55:46 +0800

    Stock and QUota comparer;

---
+++ IntelligentInvestor.Domain/Comparers/QuotaBaseComparer.cs
@@ -0,0 +1,27 @@
+﻿using IntelligentInvestor.Domain.Quotas;
+
+namespace IntelligentInvestor.Domain.Comparers;
+
+public class QuotaBaseComparer<TStockTimelyBase> : StockBaseComparer<TStockTimelyBase>
+    where TStockTimelyBase : QuotaBase
+{
+    public override int Compare(TStockTimelyBase x, TStockTimelyBase y)
+    {
+        if (ReferenceEquals(x, y)) return 0;
+        if (x != null && y == null) return 1;
+        if (x == null && y != null) return -1;
+
+        int result = base.Compare(x!, y!);
+        if (result != 0) return result;
+
+        return x!.QuotaTime > y!.QuotaTime ? 1 : -1;
+    }
+
+    public override bool Equals(TStockTimelyBase x, TStockTimelyBase y)
+    {
+        bool result = base.Equals(x, y);
+        if (!result) return result;
+
+        return x!.QuotaTime == y!.QuotaTime;
+    }
+}
---
+++ IntelligentInvestor.Domain/Comparers/StockBaseComparer.cs
@@ -0,0 +1,26 @@
+﻿using IntelligentInvestor.Domain.Stocks;
+
+namespace IntelligentInvestor.Domain.Comparers;
+
+public class StockBaseComparer<TStockBase> : IEqualityComparer<TStockBase>, IComparer<TStockBase>
+        where TStockBase : StockBase
+{
+    public virtual int Compare(TStockBase x, TStockBase y)
+    {
+        if (ReferenceEquals(x, y)) return 0;
+        if (x != null && y == null) return 1;
+        if (x == null && y != null) return -1;
+
+        if (x!.StockMarket != y!.StockMarket)
+            return x.StockMarket > y.StockMarket ? 1 : -1;
+
+        return string.Compare(x!.StockCode, y!.StockCode, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public virtual bool Equals(TStockBase x, TStockBase y)
+        => ReferenceEquals(x, y) || (x?.StockMarket == y?.StockMarket &&
+        string.Equals(x?.StockCode, y?.StockCode, StringComparison.OrdinalIgnoreCase));
+
+    public virtual int GetHashCode(TStockBase obj)
+        => obj.GetHashCode();
+}
