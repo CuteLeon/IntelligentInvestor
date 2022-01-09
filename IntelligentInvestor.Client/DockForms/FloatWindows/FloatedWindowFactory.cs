4279887c086bca7a7e1d4177ce3e9a7f97f014b2
Leon Liu <lliu@fnz.com>
2022-01-09 21:09:40 +0800

    Modification;

--- IntelligentInvestor.Client/DockForms/FloatWindows/FloatedWindowFactory.cs
+++ IntelligentInvestor.Client/DockForms/FloatWindows/FloatedWindowFactory.cs
@@ -1,16 +1,15 @@
 ﻿using WeifenLuo.WinFormsUI.Docking;
 using static WeifenLuo.WinFormsUI.Docking.DockPanelExtender;
 
-namespace IntelligentInvestor.Client.DockForms.FloatWindows
+namespace IntelligentInvestor.Client.DockForms.FloatWindows;
+
+public class FloatedWindowFactory : IFloatWindowFactory
 {
-    public class FloatedWindowFactory : IFloatWindowFactory
-    {
-        public static FloatedWindowFactory SingleInstance { get; } = new FloatedWindowFactory();
+    public static FloatedWindowFactory SingleInstance { get; } = new FloatedWindowFactory();
 
-        public FloatWindow CreateFloatWindow(DockPanel dockPanel, DockPane pane, Rectangle bounds)
-            => new FloatedWindow(dockPanel, pane, bounds);
+    public FloatWindow CreateFloatWindow(DockPanel dockPanel, DockPane pane, Rectangle bounds)
+        => new FloatedWindow(dockPanel, pane, bounds);
 
-        public FloatWindow CreateFloatWindow(DockPanel dockPanel, DockPane pane)
-            => new FloatedWindow(dockPanel, pane);
-    }
+    public FloatWindow CreateFloatWindow(DockPanel dockPanel, DockPane pane)
+        => new FloatedWindow(dockPanel, pane);
 }
