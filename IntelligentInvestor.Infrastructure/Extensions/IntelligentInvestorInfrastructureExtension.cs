02002af522e639af69ceacecbcc74b54f36c5f10
Leon Liu <lliu@fnz.com>
2022-01-09 23:23:32 +0800

    Reg Repository services into DI;

---
+++ IntelligentInvestor.Infrastructure/Extensions/IntelligentInvestorInfrastructureExtension.cs
@@ -0,0 +1,17 @@
+﻿using IntelligentInvestor.Application.Repositorys.Abstractions;
+using IntelligentInvestor.Infrastructure.DBContexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntelligentInvestor.Infrastructure.Extensions;
+
+public static class IntelligentInvestorInfrastructureExtension
+{
+    public static IServiceCollection AddIntelligentInvestorInfrastructure(this IServiceCollection services)
+        => services.AddTransient(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
+
+    public static IServiceCollection AddIntelligentInvestorDBContext(
+        this IServiceCollection services, string connectionString)
+        => services.AddDbContext<DbContext, IntelligentInvestorDBContext>(
+            options => options.UseSqlite(connectionString).UseLazyLoadingProxies());
+}
