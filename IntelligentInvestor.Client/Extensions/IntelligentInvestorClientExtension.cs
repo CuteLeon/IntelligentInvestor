b48727ec25f9728752dd7424fc1c5ba67e5cfce6
Leon Liu <lliu@fnz.com>
2022-01-09 23:18:55 +0800

    Reg forms into DI;

---
+++ IntelligentInvestor.Client/Extensions/IntelligentInvestorClientExtension.cs
@@ -0,0 +1,22 @@
+﻿using IntelligentInvestor.Client.DockForms;
+using IntelligentInvestor.Client.Themes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntelligentInvestor.Client.Extensions;
+
+public static class IntelligentInvestorClientExtension
+{
+    public static IServiceCollection AddIntelligentInvestorClient(this IServiceCollection services)
+        => services
+            .AddSingleton<IUIThemeHandler, UIThemeHandler>()
+            .AddSingleton<MainForm>()
+            .AddSingleton<RecentTradeForm>()
+            .AddSingleton<MarketQuotaForm>()
+            .AddSingleton<CurrentQuotaForm>()
+            .AddSingleton<HotStockDockForm>()
+            .AddSingleton<SearchStockDockForm>()
+            .AddSingleton<SelfSelectStockForm>()
+            .AddTransient<ChartDocumentForm>()
+            .AddTransient<RecentQuotaDocumentForm>()
+            .AddTransient<QuotaRepositoryDocumentForm>();
+}
