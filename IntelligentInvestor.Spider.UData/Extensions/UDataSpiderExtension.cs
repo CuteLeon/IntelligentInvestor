05a0d6b9ab4e263fa742df96f1050da29e713207
Leon Liu <lliu@fnz.com>
2022-01-14 14:29:15 +0800

    Add UData spider project;

---
+++ IntelligentInvestor.Spider.UData/Extensions/UDataSpiderExtension.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace IntelligentInvestor.Spider.UData.Extensions;
+
+public static class UDataSpiderExtension
+{
+    public static IServiceCollection AddIntelligentInvestorClient(this IServiceCollection services)
+        => services;
+}
