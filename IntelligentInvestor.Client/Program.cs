1715142a02f770b6de79025dae635ed238bdf625
Leon Liu <lliu@fnz.com>
2022-01-21 00:03:44 +0800

    Test 1

--- IntelligentInvestor.Client/Program.cs
+++ IntelligentInvestor.Client/Program.cs
@@ -15,149 +15,148 @@ using NLog.Extensions.Logging;
 using NLogger = NLog.Logger;
 using WinApplication = System.Windows.Forms.Application;
 
-namespace IntelligentInvestor.Client
+namespace IntelligentInvestor.Client;
+break
+internal static class Program
 {
-    internal static class Program
+    public static ProgramHost Host { get; } = new ProgramHost();
+
+    public static NLogger Logger { get; private set; }
+
+    [STAThread]
+    static void Main()
     {
-        public static ProgramHost Host { get; } = new ProgramHost();
+        Logger = NLog.LogManager.LoadConfiguration("NLog.config").GetCurrentClassLogger();
+        WinApplication.EnableVisualStyles();
+        WinApplication.SetCompatibleTextRenderingDefault(false);
+        WinApplication.SetHighDpiMode(HighDpiMode.SystemAware);
 
-        public static NLogger Logger { get; private set; }
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        WinApplication.ThreadException += Application_ThreadException;
+        WinApplication.ApplicationExit += Application_ApplicationExit;
 
-        [STAThread]
-        static void Main()
+        using (var launchForm = new LaunchForm(InitializeProgramHost))
         {
-            Logger = NLog.LogManager.LoadConfiguration("NLog.config").GetCurrentClassLogger();
-            WinApplication.EnableVisualStyles();
-            WinApplication.SetCompatibleTextRenderingDefault(false);
-            WinApplication.SetHighDpiMode(HighDpiMode.SystemAware);
+            var result = launchForm.ShowDialog();
+            Logger.Debug($"Launch dialog result is {result}.");
+            if (result != DialogResult.OK) return;
+        }
 
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-            WinApplication.ThreadException += Application_ThreadException;
-            WinApplication.ApplicationExit += Application_ApplicationExit;
+        var mainForm = Host.ServiceProvider.GetRequiredService<MainForm>();
+        WinApplication.Run(mainForm);
+        Logger.Debug("Application message loop collected.");
+    }
 
-            using (var launchForm = new LaunchForm(InitializeProgramHost))
+    static async IAsyncEnumerable<string> InitializeProgramHost()
+    {
+        Logger.Debug("Initialize program host ...");
+        yield return "Initialize program host ...";
+
+        foreach (var message in Host.InitializeConfigurationManager()
+            .Concat(Host.InitializeServiceProvider())
+            .Concat(Host.InitializeStockSpider())
+            .Concat(Host.InitializeIntermediary())
+            .Concat(Host.BuilderServiceProvider()))
+            yield return message;
+
+        await foreach (var message in Host.InitializeDatabase())
+            yield return message;
+
+        Logger.Debug("Initialize successfully.");
+        yield return "Initialize successfully.";
+    }
+
+    static IEnumerable<string> InitializeConfigurationManager(this ProgramHost host)
+    {
+        Logger.Debug("Initialize configuration manager ...");
+        yield return "Initialize configuration manager ...";
+        host.Configuration.AddJsonFile("AppSettings.json", true, true);
+        host.Services.AddSingleton(host.Configuration);
+    }
+
+    static IEnumerable<string> InitializeServiceProvider(this ProgramHost host)
+    {
+        Logger.Debug("Initialize service provider ...");
+        yield return "Initialize service provider ...";
+        var services = host.Services
+            .AddIntelligentInvestorClient()
+            .AddIntelligentInvestorIntermediary()
+            .AddIntelligentInvestorDBContext(host.Configuration.GetConnectionString("IIDB"))
+            .AddIntelligentInvestorInfrastructure();
+
+        services.AddLogging(builder => builder.ClearProviders().AddNLog(new NLogProviderOptions()).SetMinimumLevel(LogLevel.Trace));
+    }
+
+    static async IAsyncEnumerable<string> InitializeDatabase(this ProgramHost host)
+    {
+        Logger.Debug("Initialize database ...");
+        yield return "Initialize database ...";
+
+        using var scope = host.ServiceProvider.CreateScope();
+        var services = scope.ServiceProvider;
+        var dbContext = services.GetRequiredService<DbContext>();
+
+        try
+        {
+            Logger.Debug($"Ensure database created...");
+            await dbContext.Database.EnsureCreatedAsync();
+            Logger.Debug($"Check database pending migrations...");
+            var pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
+            if (pendingMigrations.Any())
             {
-                var result = launchForm.ShowDialog();
-                Logger.Debug($"Launch dialog result is {result}.");
-                if (result != DialogResult.OK) return;
+                Logger.Info($"Pending database migration should be combined: \n\t{string.Join(",", pendingMigrations)}");
+                await dbContext.Database.MigrateAsync();
             }
-
-            var mainForm = Host.ServiceProvider.GetRequiredService<MainForm>();
-            WinApplication.Run(mainForm);
-            Logger.Debug("Application message loop collected.");
+            Logger.Debug($"Database check finished.");
         }
-
-        static async IAsyncEnumerable<string> InitializeProgramHost()
+        catch (Exception ex)
         {
-            Logger.Debug("Initialize program host ...");
-            yield return "Initialize program host ...";
-
-            foreach (var message in Host.InitializeConfigurationManager()
-                .Concat(Host.InitializeServiceProvider())
-                .Concat(Host.InitializeStockSpider())
-                .Concat(Host.InitializeIntermediary())
-                .Concat(Host.BuilderServiceProvider()))
-                yield return message;
-
-            await foreach (var message in Host.InitializeDatabase())
-                yield return message;
-
-            Logger.Debug("Initialize successfully.");
-            yield return "Initialize successfully.";
+            Logger.Error(ex, $"Database check failed.");
         }
+    }
 
-        static IEnumerable<string> InitializeConfigurationManager(this ProgramHost host)
-        {
-            Logger.Debug("Initialize configuration manager ...");
-            yield return "Initialize configuration manager ...";
-            host.Configuration.AddJsonFile("AppSettings.json", true, true);
-            host.Services.AddSingleton(host.Configuration);
-        }
+    static IEnumerable<string> InitializeStockSpider(this ProgramHost host)
+    {
+        Logger.Debug("Initialize stock spider ...");
+        yield return "Initialize stock spider ...";
+        var spiderOptions = Host.Configuration.GetSection("Spider");
+        var services = host.Services
+            .AddMockSpider()
+            .AddSinaSpider(spiderOptions.GetSection("Sina").Get<SpiderOption>())
+            .AddUDataSpider(spiderOptions.GetSection("UData").Get<SpiderOption>());
+    }
 
-        static IEnumerable<string> InitializeServiceProvider(this ProgramHost host)
-        {
-            Logger.Debug("Initialize service provider ...");
-            yield return "Initialize service provider ...";
-            var services = host.Services
-                .AddIntelligentInvestorClient()
-                .AddIntelligentInvestorIntermediary()
-                .AddIntelligentInvestorDBContext(host.Configuration.GetConnectionString("IIDB"))
-                .AddIntelligentInvestorInfrastructure();
+    static IEnumerable<string> InitializeIntermediary(this ProgramHost host)
+    {
+        Logger.Debug("Initialize intermediary ...");
+        yield return "Initialize intermediary ...";
+        host.Services
+            .AddIntermediaryEvent<StockEvent>()
+            .AddIntermediaryEvent<ThemeEvent>();
+    }
 
-            services.AddLogging(builder => builder.ClearProviders().AddNLog(new NLogProviderOptions()).SetMinimumLevel(LogLevel.Trace));
-        }
+    private static void Application_ApplicationExit(object sender, EventArgs e)
+    {
+        Logger.Debug($"Application exits.");
+    }
 
-        static async IAsyncEnumerable<string> InitializeDatabase(this ProgramHost host)
-        {
-            Logger.Debug("Initialize database ...");
-            yield return "Initialize database ...";
+    private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+    {
+        Logger.Error(e.Exception, "Applicarion error.");
+        MessageBox.Show(
+            $"Application error: \n{e.Exception.Message}",
+            "Application Error",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Error);
+    }
 
-            using var scope = host.ServiceProvider.CreateScope();
-            var services = scope.ServiceProvider;
-            var dbContext = services.GetRequiredService<DbContext>();
-
-            try
-            {
-                Logger.Debug($"Ensure database created...");
-                await dbContext.Database.EnsureCreatedAsync();
-                Logger.Debug($"Check database pending migrations...");
-                var pendingMigrations = await dbContext.Database.GetPendingMigrationsAsync();
-                if (pendingMigrations.Any())
-                {
-                    Logger.Info($"Pending database migration should be combined: \n\t{string.Join(",", pendingMigrations)}");
-                    await dbContext.Database.MigrateAsync();
-                }
-                Logger.Debug($"Database check finished.");
-            }
-            catch (Exception ex)
-            {
-                Logger.Error(ex, $"Database check failed.");
-            }
-        }
-
-        static IEnumerable<string> InitializeStockSpider(this ProgramHost host)
-        {
-            Logger.Debug("Initialize stock spider ...");
-            yield return "Initialize stock spider ...";
-            var spiderOptions = Host.Configuration.GetSection("Spider");
-            var services = host.Services
-                .AddMockSpider()
-                .AddSinaSpider(spiderOptions.GetSection("Sina").Get<SpiderOption>())
-                .AddUDataSpider(spiderOptions.GetSection("UData").Get<SpiderOption>());
-        }
-
-        static IEnumerable<string> InitializeIntermediary(this ProgramHost host)
-        {
-            Logger.Debug("Initialize intermediary ...");
-            yield return "Initialize intermediary ...";
-            host.Services
-                .AddIntermediaryEvent<StockEvent>()
-                .AddIntermediaryEvent<ThemeEvent>();
-        }
-
-        private static void Application_ApplicationExit(object sender, EventArgs e)
-        {
-            Logger.Debug($"Application exits.");
-        }
-
-        private static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
-        {
-            Logger.Error(e.Exception, "Applicarion error.");
-            MessageBox.Show(
-                $"Application error: \n{e.Exception.Message}",
-                "Application Error",
-                MessageBoxButtons.OK,
-                MessageBoxIcon.Error);
-        }
-
-        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
-        {
-            Logger.Error(e.ExceptionObject as Exception, $"Domain error.");
-            MessageBox.Show(
-                $"Domain error: \n{(e.ExceptionObject as Exception)?.Message}",
-                "Domain Error",
-                MessageBoxButtons.OK,
-                e.IsTerminating ? MessageBoxIcon.Error : MessageBoxIcon.Warning);
-        }
+    private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        Logger.Error(e.ExceptionObject as Exception, $"Domain error.");
+        MessageBox.Show(
+            $"Domain error: \n{(e.ExceptionObject as Exception)?.Message}",
+            "Domain Error",
+            MessageBoxButtons.OK,
+            e.IsTerminating ? MessageBoxIcon.Error : MessageBoxIcon.Warning);
     }
 }
